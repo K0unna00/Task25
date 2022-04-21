1ae3b746a2412ff42c54969e2ece6ae900e6ad54
K0unn00 <78pf4jz@code.edu.az>
2022-04-21 14:17:35 +0400

    Add project files.

---
+++ MenuCs/Data/SQL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace MenuCs.Data
+{
+    internal class SQL
+    {
+        public static string ConnString = @"Server=BEXININSIRINNOT\SQLEXPRESS;Database=Task25;Trusted_Connection=True;";
+    }
+}
---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace Task25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection(@"Server=BEXININSIRINNOT\SQLEXPRESS;Database=Task15;Trusted_Connection=True");
+
+            string sql = "insert Into Stadiums values ('Stadium 1', 10, 11), ('Stadium 2', 15, 11), ('Stadium 3', 20, 11)";
+            conn.Open();
+            SqlCommand sqlCMD = new SqlCommand(sql, conn);
+            sqlCMD.ExecuteNonQuery();
+            conn.Close();
+
+        }
+    }
+}
